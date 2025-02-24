e624c6289f55a09c8661c01c982e0e1cbee2a476
roshandec29 <roshandec252@gmail.com>
2025-02-25 01:53:05 +0530

    structuring

--- app/api/v1/inventory.py
+++ app/api/v1/inventory.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from app.db.session import DBSync
 from app.services.inventory.schema import CategoryResponse, ItemResponse, ItemCreate, CategoryCreate
-from app.services.inventory.models.menu import Item, Category
+from app.services.inventory.inventory_service import CategoryService, ItemService
 
 
 router = APIRouter()
@@ -12,57 +12,6 @@ def get_session():
     return DBSync().get_new_session()
 
 
-class CategoryService:
-    def __init__(self, session: Session):
-        self.session = session
-
-    def create_category(self, category_data: CategoryCreate):
-        category = Category(**category_data.dict())
-        self.session.add(category)
-        self.session.commit()
-        self.session.refresh(category)
-        return category
-
-    def get_categories(self):
-        return self.session.query(Category).all()
-
-    def get_category(self, category_id: int):
-        category = self.session.query(Category).filter(Category.id == category_id).first()
-        if not category:
-            raise HTTPException(status_code=404, detail="Category not found")
-        return category
-
-    def delete_category(self, category_id: int):
-        category = self.get_category(category_id)
-        self.session.delete(category)
-        self.session.commit()
-        return {"message": "Category deleted successfully"}
-
-class ItemService:
-    def __init__(self, session: Session):
-        self.session = session
-
-    def create_item(self, item_data: ItemCreate):
-        item = Item(**item_data.dict())
-        self.session.add(item)
-        self.session.commit()
-        self.session.refresh(item)
-        return item
-
-    def get_items(self):
-        return self.session.query(Item).all()
-
-    def get_item(self, item_id: int):
-        item = self.session.query(Item).filter(Item.id == item_id).first()
-        if not item:
-            raise HTTPException(status_code=404, detail="Item not found")
-        return item
-
-    def delete_item(self, item_id: int):
-        item = self.get_item(item_id)
-        self.session.delete(item)
-        self.session.commit()
-        return {"message": "Item deleted successfully"}
 
 # Routes
 @router.post("/categories/", response_model=CategoryResponse)
--- app/services/inventory/inventory_service.py
+++ app/services/inventory/inventory_service.py
@@ -1,6 +1,58 @@
 from sqlalchemy.orm import Session
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import HTTPException
+from app.services.inventory.models.menu import Item, Category
+from app.services.inventory.schema import CategoryResponse, ItemResponse, ItemCreate, CategoryCreate
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+class CategoryService:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def create_category(self, category_data: CategoryCreate):
+        category = Category(**category_data.model_dump())
+        self.session.add(category)
+        self.session.commit()
+        self.session.refresh(category)
+        return category
+
+    def get_categories(self):
+        return self.session.query(Category).all()
+
+    def get_category(self, category_id: int):
+        category = self.session.query(Category).filter(Category.id == category_id).first()
+        if not category:
+            raise HTTPException(status_code=404, detail="Category not found")
+        return category
+
+    def delete_category(self, category_id: int):
+        category = self.get_category(category_id)
+        self.session.delete(category)
+        self.session.commit()
+        return {"message": "Category deleted successfully"}
+
+
+class ItemService:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def create_item(self, item_data: ItemCreate):
+        item = Item(**item_data.model_dump())
+        self.session.add(item)
+        self.session.commit()
+        self.session.refresh(item)
+        return item
+
+    def get_items(self):
+        return self.session.query(Item).all()
+
+    def get_item(self, item_id: int):
+        item = self.session.query(Item).filter(Item.id == item_id).first()
+        if not item:
+            raise HTTPException(status_code=404, detail="Item not found")
+        return item
+
+    def delete_item(self, item_id: int):
+        item = self.get_item(item_id)
+        self.session.delete(item)
+        self.session.commit()
+        return {"message": "Item deleted successfully"}
